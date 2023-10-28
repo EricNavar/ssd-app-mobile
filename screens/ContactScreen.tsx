52ee4696f1154d1459279614305bec6e01218a2b
ericnavar <ericn2413@gmail.com>
2023-10-28 18:24:55 -0400

    Add link to SSD's linkedin

--- screens/ContactScreen.tsx
+++ screens/ContactScreen.tsx
@@ -12,12 +12,13 @@ const ContactScreen = () => {
         'Discord': require('../assets/icons/discord.png'),
         'Youtube': require('../assets/icons/youtube.png'),
         'Instagram': require('../assets/icons/instagram.png'),
+        'LinkedIn': require('../assets/icons/linkedin.png'),
     };
 
     return (
         <Layout style={{paddingHorizontal: 20}}>
             <View>
-                <View style={{display:'flex', flexDirection:'row', marginBottom: 20}}>
+                <View marginB-20 >
                     <Text text70>Shoot us an email and our President Yonas Bahre or the Ourtrach Officer Angel will reply: </Text>
                     <Button link label='ufssd.org@gmail.com' onPress={() => onPressSocial('ufssd.org@gmail.com')}/>
                 </View>
@@ -25,18 +26,28 @@ const ContactScreen = () => {
                     label='Discord'
                     onPress={() => onPressSocial('https://discord.gg/5SyB3yx')}
                     iconSource={icons.Discord}
+                    iconStyle={{height:24,width:24}}
                     marginB-20
                 />
                 <Button
                     label='YouTube'
                     onPress={() => onPressSocial('https://www.youtube.com/@ufssd')}
                     iconSource={icons.Youtube}
+                    iconStyle={{height:24,width:24}}
                     marginB-20
                 />
                 <Button
                     label='Instagram'
                     onPress={() => onPressSocial('https://www.instagram.com/uf.ssd/')}
                     iconSource={icons.Instagram}
+                    iconStyle={{height:24,width:24}}
+                    marginB-20
+                />
+                <Button
+                    label='LinkedIn'
+                    onPress={() => onPressSocial('https://www.linkedin.com/company/ssduf?trk=public_post_feed-actor-name')}
+                    iconSource={icons.LinkedIn}
+                    iconStyle={{height:24,width:24}}
                     marginB-20
                 />
             </View>
